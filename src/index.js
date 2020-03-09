a846138f66caecbdcf8e4eef3cc3e1b1c5cc694e
aidan <aidang4@uw.edu>
2020-03-09 16:12:27 -0700

    prevented map from updating on slider change if year didn't actually change, cleaned up code and tooltip

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ var obj = require("./election-data.json");
 //console.log(obj.length);
 
 var voteData = new Map();
-
+// we are now keeping track of the year as a public variable, so we only update
+// The map when the actual year changes
+var publicYear = 0;
 var map = new Map();
 var loserMap = new Map();
 var candidateMap = new Map();
@@ -29,18 +31,15 @@ for (var i = 0; i < obj.length; i++) {
     if (!map2.has(state)) {
         map2.set(state, votes);
     } else if (!loserMap2.has(state)) {
-    	loserMap2.set(state, votes);
+        loserMap2.set(state, votes);
     }
-    /*else if (!loserMap2.has(state)) {
-    	loserMap2.set(state, votes)
-    }*/
     map.set(year, map2);
     loserMap.set(year, loserMap2);
     if(!partyMap.has(obj[i].party)){
-    	partyMap.set(obj[i].party, {
-    		name: obj[i].candidate,
-    		votes: 0
-    	});
+        partyMap.set(obj[i].party, {
+            name: obj[i].candidate,
+            votes: 0
+        });
     }
     partyMap.get(obj[i].party).votes += obj[i].candidatevotes;
     candidateMap.set(year, partyMap);
@@ -55,108 +54,51 @@ var timeouts = [];
 //var title = document.getElementById("title-container");
 //console.log(title.textContent);
 //console.log(sliderTime.value().getYear() + 1900);
-function tooltipHtml(n, d){	/* function to create html content string in tooltip div. */
+function tooltipHtml(n, d){ /* function to create html content string in tooltip div. */
     return "<h4>"+n+"</h4><table>"+
         "<tr><td>Party</td><td>"+"Vote (Pct.)"+"</td></tr>"+
-        "<tr><td>Democrat</td><td>"+(d.low)+"</td></tr>"+
-        "<tr><td>Republican</td><td>"+(d.high)+"</td></tr>"+
-        "<tr><td>Other</td><td>"+(d.avg)+"</td></tr>"+
+        "<tr><td>Democrat</td><td>"+(d.dem)+"</td></tr>"+
+        "<tr><td>Republican</td><td>"+(d.rep)+"</td></tr>"+
+        "<tr><td>Other</td><td>"+(d.other)+"</td></tr>"+
         "</table>";
-}
-
-const title = document.getElementById('title-container').addEventListener('change', populateMap);
-
-function populateMap() {
-
-    var sampleData ={}; /* Sample random data. */   
-
-    var currentYear = sliderTime.value().getYear() + 1900;
-    ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-    "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-    "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-    "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-    "WI", "MO", "AR", "OK", "KS", "LA", "VA"]
-        .forEach(function(d){
-            var map2 = map.get(currentYear);
-            var loserMap2 = loserMap.get(currentYear);
-            //console.log(map2);
-            //console.log(map2);
-            //console.log(map2.get(d));
-            var info = map2.get(d);
-            var info2 = loserMap2.get(d);
-            //x console.log(d);
-            //console.log(info);
-            //console.log(d);
-            //console.log(d);
-            //console.log(info);
-            var party = info[2];
-            var votes = info[0];
-            var totalvotes = info[1];
-            var loserVotes = info2[0];
-            var winningPercent = (1.0 * votes) / (votes + loserVotes);// fraction only including repub and dem
-            winningPercent = Math.min(1, 4*(winningPercent - .42));
-            // winning percent is simply a ratio to decide coloring, not actual winning percent
-            var otherVotes = totalvotes - votes - loserVotes;
-            var winPercent = Math.round((10000.0 * votes) / (totalvotes)) / 100; // this fraction includes other
-            var losePercent = Math.round((10000.0 * loserVotes) / (totalvotes)) / 100;
-            var otherPercent = Math.round((10000.0 * otherVotes) / (totalvotes)) / 100; 
-            // LOW = the democrat votes, HIGH = the republican votes AVG = other votes
-            if(party == "democrat") {
-                /*var low=Math.round(50 + 50*Math.random()), 
-                    mid=Math.round(50 + 50*Math.random()), 
-                    high=Math.round(50 + 50*Math.random());
-                sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-                        avg:Math.round((low+mid+high)/3), color:d3.interpolate("#FFFFFF", "#0015BC")((low/100))}; 
-                */
-                sampleData[d] = {low:winPercent, high:losePercent, avg:otherPercent, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
-                //sampleData[d] = {low:votes, high:loserVotes, avg:otherVotes, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
-            } else {
-                /*var low=Math.round(50*Math.random()), 
-                    mid=Math.round(50*Math.random()), 
-                    high=Math.round(50*Math.random());
-                sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-                        avg:Math.round((low+mid+high)/3), color:d3.interpolate("#E9141D", "#FFFFFF")((low/100))}; 
-                */
-                sampleData[d] = {low:losePercent, high:winPercent, avg:otherPercent, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
-            }
-        });
-    /* draw states on id #statesvg */   
-    //console.log(sampleData);
-    //setTimeout(function () {
-    uStates.draw("#statesvg", sampleData, tooltipHtml);
-    //}, 1500);
-   // d3.select(self.frameElement).style("height", "600px");
 }
 
 function sliderChange(val){
     for (var i = 0; i < timeouts.length; i++) {
         clearTimeout(timeouts[i]);
     }
-
-    d3.select('#title-container').text("US Election Results in "+d3.timeFormat('%Y')(sliderTime.value()));
+    //console.log(publicYear);
+    //var beforeChangeYear = 
     var currentYear = sliderTime.value().getYear() + 1900;
-  	  d3.select('#key-title').text("Major Candidates (Winner in Bold)");
-  	  d3.select('#republican-container').text(candidateMap.get(currentYear).get("republican").name);
+    // only do stuff if the year has changed
+    if (publicYear != currentYear) {
+      //d3.select('#title-container').text("US Election Results in "+d3.timeFormat('%Y')(sliderTime.value()));
+      d3.select('#title-container').text("US Election Results in "+ currentYear);
+      publicYear = currentYear;
+      d3.select('#key-title').text("Major Candidates (Winner in Bold)");
+      d3.select('#republican-container').text(candidateMap.get(currentYear).get("republican").name);
       d3.select('#democrat-container').text(candidateMap.get(currentYear).get("democrat").name);
       //console.log('made a call to on change, about to populate map');
       // lost popular vote ut won election
-	  if (currentYear == 2000 || currentYear == 2016) {
-			d3.select('#republican-container').text(candidateMap.get(currentYear).get("republican").name + "*");
-			d3.select('#republican-container').style("font-weight", 900);
-      		d3.select('#democrat-container').style("font-weight", 100);
-      		d3.select('#asterisk-container').text('* = Lost Popular Vote');
-	  }
+      if (currentYear == 2000 || currentYear == 2016) {
+            d3.select('#republican-container').text(candidateMap.get(currentYear).get("republican").name + "*");
+            d3.select('#republican-container').style("font-weight", 900);
+            d3.select('#democrat-container').style("font-weight", 100);
+            d3.select('#asterisk-container').text('* = Lost Popular Vote');
+      }
       else if(candidateMap.get(currentYear).get("republican").votes > candidateMap.get(currentYear).get("democrat").votes){
-      		d3.select('#republican-container').style("font-weight", 900);
-      		d3.select('#democrat-container').style("font-weight", 100);
-      		d3.select('#asterisk-container').text('');
+            d3.select('#republican-container').style("font-weight", 900);
+            d3.select('#democrat-container').style("font-weight", 100);
+            d3.select('#asterisk-container').text('');
 
       } else {
-      		d3.select('#democrat-container').style("font-weight", 900);
-      		d3.select('#republican-container').style("font-weight", 100);
-      		d3.select('#asterisk-container').text('');
+            d3.select('#democrat-container').style("font-weight", 900);
+            d3.select('#republican-container').style("font-weight", 100);
+            d3.select('#asterisk-container').text('');
       }
-	  populateMap();
+      //populateMap();
+      popMapWithYear(currentYear);
+  }
 }
 
 sliderTime.on('onchange', sliderChange);
@@ -164,61 +106,50 @@ sliderChange();
 
 // panoramic view logic
 function popMapWithYear(currentYear) {
-    var sampleData ={};	/* Sample random data. */	
+    var sampleData ={}; /* Sample random data. */   
     let titleVar = document.getElementById("title-container");
     titleVar.innerHTML = "US Election Results in " + currentYear;
-
-    //var currentYear = sliderTime.value().getYear() + 1900;
-    //console.log(currentYear);
-    ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-    "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-    "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-    "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-    "WI", "MO", "AR", "OK", "KS", "LA", "VA"]
-        .forEach(function(d){
-            var map2 = map.get(currentYear);
-            var loserMap2 = loserMap.get(currentYear);
-            //console.log(map2);
-            //console.log(map2);
-            //console.log(map2.get(d));
-            var info = map2.get(d);
-            var info2 = loserMap2.get(d);
-            //x	console.log(d);
-            //console.log(info);
-            //console.log(d);
-            //console.log(d);
-            //console.log(info);
-            var party = info[2];
-            var votes = info[0];
-            var totalvotes = info[1];
-            var loserVotes = info2[0];
-            var winningPercent = (1.0 * votes) / (votes + loserVotes);
-            winningPercent = Math.min(1, 4*(winningPercent - .42));
-            // winning percent is simply a ratio to decide coloring, not actual winning percent
-            var otherVotes = totalvotes - votes - loserVotes;
-            //console.log(party);
-            if(party == "democrat") {
-                /*var low=Math.round(50 + 50*Math.random()), 
-                    mid=Math.round(50 + 50*Math.random()), 
-                    high=Math.round(50 + 50*Math.random());
-                sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-                        avg:Math.round((low+mid+high)/3), color:d3.interpolate("#FFFFFF", "#0015BC")((low/100))}; 
-                */
-                sampleData[d] = {low:votes, high:loserVotes, avg:otherVotes, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
-            } else {
-                /*var low=Math.round(50*Math.random()), 
-                    mid=Math.round(50*Math.random()), 
-                    high=Math.round(50*Math.random());
-                sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-                        avg:Math.round((low+mid+high)/3), color:d3.interpolate("#E9141D", "#FFFFFF")((low/100))}; 
-                */
-                sampleData[d] = {low:loserVotes, high:votes, avg:otherVotes, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
-            }
-        });
-    /* draw states on id #statesvg */	
+    // idk if this if statement is actually necessary/currently it is not being used
+    if (voteData.has(currentYear)) {
+        console.log("TRUE");
+        uStates.draw("#statesvg", voteData.get(currentYear), tooltipHtml);
+    } else {
+        //var currentYear = sliderTime.value().getYear() + 1900;
+        //console.log(currentYear);
+        ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+        "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
+        "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
+        "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
+        "WI", "MO", "AR", "OK", "KS", "LA", "VA"]
+            .forEach(function(d){
+                var map2 = map.get(currentYear);
+                var loserMap2 = loserMap.get(currentYear);
+                var info = map2.get(d);
+                var info2 = loserMap2.get(d);
+                var party = info[2];
+                var votes = info[0];
+                var totalvotes = info[1];
+                var loserVotes = info2[0];
+                var winningPercent = (1.0 * votes) / (votes + loserVotes);// fraction only including repub and dem
+                winningPercent = Math.min(1, 4*(winningPercent - .42));
+                // winning percent is simply a ratio to decide coloring, not actual winning percent
+                var otherVotes = totalvotes - votes - loserVotes;
+                var winPercent = Math.round((10000.0 * votes) / (totalvotes)) / 100; // this fraction includes other
+                var losePercent = Math.round((10000.0 * loserVotes) / (totalvotes)) / 100;
+                var otherPercent = Math.round((10000.0 * otherVotes) / (totalvotes)) / 100; 
+                // LOW = the democrat votes, HIGH = the republican votes AVG = other votes
+                if(party == "democrat") {
+                    sampleData[d] = {dem:winPercent, rep:losePercent, other:otherPercent, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
+                } else {
+                    sampleData[d] = {dem:losePercent, rep:winPercent, other:otherPercent, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
+                }
+            });
+    /* draw states on id #statesvg */   
     //console.log(sampleData);
     //setTimeout(function () {
-    uStates.draw("#statesvg", sampleData, tooltipHtml);
+        //voteData.set(currentYear, sampleData);
+        uStates.draw("#statesvg", sampleData, tooltipHtml);
+    }
     //}, 1500);
    // d3.select(self.frameElement).style("height", "600px");
 }
