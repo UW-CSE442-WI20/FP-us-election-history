01952150bc1862ec575cba34119f5e5c99dd8864
aidan <aidang4@uw.edu>
2020-03-09 22:47:29 -0700

    finished all tooltip related stuff, added third parties, small bug w/ votes split between different parties for same candidate to fix

--- src/index.js
+++ src/index.js
@@ -9,19 +9,23 @@ var voteData = new Map();
 var publicYear = 0;
 var map = new Map();
 var loserMap = new Map();
+var thirdPlaceMap = new Map();
 var candidateMap = new Map();
 for (var i = 0; i < obj.length; i++) {
     var year = obj[i].year;
     var map2;
+    var map3;
     var partyMap;
     var loserMap2;
     if (!map.has(year)) {
         // insert year with votes data
-        partyMap = new Map();
-        loserMap2 = new Map();
-        map2 = new Map();
+        partyMap = new Map(); //party map is the candidates and their parties
+        loserMap2 = new Map();// losermap 2 is a local version of the second place map
+        map2 = new Map();// map 2 is a local version of the winner map
+        map3 = new Map();// map 3 is a local version of the third place maap
     } else {
         map2 = map.get(year); 
+        map3 = thirdPlaceMap.get(year);
         loserMap2 = loserMap.get(year);
         partyMap = candidateMap.get(year);
         partyMap.get(obj[i].party)
@@ -32,9 +36,12 @@ for (var i = 0; i < obj.length; i++) {
         map2.set(state, votes);
     } else if (!loserMap2.has(state)) {
         loserMap2.set(state, votes);
+    } else if (!map3.has(state)) {
+        map3.set(state, votes);
     }
     map.set(year, map2);
     loserMap.set(year, loserMap2);
+    thirdPlaceMap.set(year, map3);
     if(!partyMap.has(obj[i].party)){
         partyMap.set(obj[i].party, {
             name: obj[i].candidate,
@@ -55,12 +62,28 @@ var timeouts = [];
 //console.log(title.textContent);
 //console.log(sliderTime.value().getYear() + 1900);
 function tooltipHtml(n, d){ /* function to create html content string in tooltip div. */
-    return "<h4>"+n+"</h4><table>"+
-        "<tr><td>Party</td><td>"+"Vote (Pct.)"+"</td></tr>"+
-        "<tr><td>Democrat</td><td>"+(d.dem)+"</td></tr>"+
-        "<tr><td>Republican</td><td>"+(d.rep)+"</td></tr>"+
-        "<tr><td>Other</td><td>"+(d.other)+"</td></tr>"+
+    if (d.partyCount === 2) {
+        return "<h4>"+n+"</h4><table>"+
+        "<tr><td> Candidate </td><td>Party</td><td> Total Votes </td><td> Pct. </td></tr>"+
+        "<tr><td>" + d.demName + "</td><td>Democrat</td><td>"+(d.demVotes)+"</td><td>"+(d.dem)+"</td></tr>"+
+        "<tr><td>" + d.repName + "</td><td>Republican</td><td>"+(d.repVotes)+"</td><td>"+(d.rep)+"</td></tr>"+
         "</table>";
+    } else if (d.partyCount === 3) {
+        return "<h4>"+n+"</h4><table>"+
+        "<tr><td> Candidate </td><td>Party</td><td> Total Votes </td><td> Pct. </td></tr>"+
+        "<tr><td>" + d.demName + "</td><td>Democrat</td><td>"+(d.demVotes)+"</td><td>"+(d.dem)+"</td></tr>"+
+        "<tr><td>" + d.repName + "</td><td>Republican</td><td>"+(d.repVotes)+"</td><td>"+(d.rep)+"</td></tr>"+
+        "<tr><td>" + d.otherName + "</td><td>"+(d.thirdPartyOne)+"</td><td>"+(d.thirdPartyVotes)+"</td><td>"+(d.thirdVotes)+"</td></tr>"+
+        "</table>";
+    } else {
+        return "<h4>"+n+"</h4><table>"+
+        "<tr><td> Candidate </td><td>Party</td><td> Total Votes </td><td> Pct. </td></tr>"+
+        "<tr><td>" + d.demName + "</td><td>Democrat</td><td>"+(d.demVotes)+"</td><td>"+(d.dem)+"</td></tr>"+
+        "<tr><td>" + d.repName + "</td><td>Republican</td><td>"+(d.repVotes)+"</td><td>"+(d.rep)+"</td></tr>"+
+        "<tr><td>" + d.otherName + "</td><td>"+(d.thirdPartyOne)+"</td><td>"+(d.thirdPartyVotes)+"</td><td>"+(d.thirdVotes)+"</td></tr>"+
+        "<tr><td>Other</td><td> N/A </td><td>"+(d.otherVotes)+"</td><td>"+(d.other)+"</td></tr>"+
+        "</table>";
+    }
 }
 
 function sliderChange(val){
@@ -116,32 +139,71 @@ function popMapWithYear(currentYear) {
     } else {
         //var currentYear = sliderTime.value().getYear() + 1900;
         //console.log(currentYear);
+        // map 2 = the winners
+        var map2 = map.get(currentYear);
+        // loser map 2 = the second place
+        var loserMap2 = loserMap.get(currentYear);
+        // iinfo and info 2 i believe are the results for a specific state
+        var thirdPlace2 = thirdPlaceMap.get(currentYear);
         ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
         "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
         "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
         "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
         "WI", "MO", "AR", "OK", "KS", "LA", "VA"]
             .forEach(function(d){
-                var map2 = map.get(currentYear);
-                var loserMap2 = loserMap.get(currentYear);
                 var info = map2.get(d);
                 var info2 = loserMap2.get(d);
-                var party = info[2];
-                var votes = info[0];
-                var totalvotes = info[1];
-                var loserVotes = info2[0];
-                var winningPercent = (1.0 * votes) / (votes + loserVotes);// fraction only including repub and dem
+                var info3 = thirdPlace2.get(d);
+                var party = info[2]; // party of winner
+                var votes = info[0]; // raw votes of winner
+                var totalvotes = info[1]; // total overall votes
+                var loserVotes = info2[0]; // raw votes of second place
+                var winningPercent = (1.0 * votes) / (votes + loserVotes);// fraction only including repub and dem (top 2)
                 winningPercent = Math.min(1, 4*(winningPercent - .42));
                 // winning percent is simply a ratio to decide coloring, not actual winning percent
-                var otherVotes = totalvotes - votes - loserVotes;
-                var winPercent = Math.round((10000.0 * votes) / (totalvotes)) / 100; // this fraction includes other
-                var losePercent = Math.round((10000.0 * loserVotes) / (totalvotes)) / 100;
-                var otherPercent = Math.round((10000.0 * otherVotes) / (totalvotes)) / 100; 
-                // LOW = the democrat votes, HIGH = the republican votes AVG = other votes
-                if(party == "democrat") {
-                    sampleData[d] = {dem:winPercent, rep:losePercent, other:otherPercent, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
-                } else {
-                    sampleData[d] = {dem:losePercent, rep:winPercent, other:otherPercent, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
+                var winPercent = Math.round((1000.0 * votes) / (totalvotes)) / 10; // this fraction includes other
+                var losePercent = Math.round((1000.0 * loserVotes) / (totalvotes)) / 10;
+                // IF THERE ARE ONLY 2 PARTIES WE ONLY DISPLAY TWO PARTIES    
+                var tempName = info[3].split(",");
+                var winnerName = tempName[1] + " " + tempName[0];
+                tempName = info2[3].split(",");
+                var loserName = tempName[1] + " " + tempName[0];
+                if (info3 == null) {
+                    if(party == "democrat") {
+                        sampleData[d] = {demName:winnerName, repName:loserName, partyCount:2, dem:winPercent, rep:losePercent, demVotes:votes, repVotes:loserVotes, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
+                    } else {
+                        sampleData[d] = {demName:loserName, repName:winnerName, partyCount:2, dem:losePercent, rep:winPercent, demVotes:loserVotes, repVotes:votes, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
+                    }                
+                }
+                else {
+                    var thirdVotes = info3[0];
+                    var otherVotes = totalvotes - votes - loserVotes - thirdVotes;
+                    var thirdPlacePercent = Math.round((1000.0 * thirdVotes) / (totalvotes)) / 10;
+                    var otherPercent = Math.round((1000.0 * otherVotes) / (totalvotes)) / 10; 
+                    var thirdParty = info3[2].charAt(0).toUpperCase() + info3[2].substring(1);
+                    var otherName;
+                    if (info3[3] === 'Other' || info3[3] === '') {
+                        otherName = 'Other';
+                        thirdVotes += otherVotes;
+                        thirdParty = 'N/A';
+                        otherVotes = 0;
+                    } else {
+                        tempName = info3[3].split(",");
+                        otherName = tempName[1] + " " + tempName[0];
+                    }
+                    if (otherVotes === 0) {
+                        if(party == "democrat") {
+                            sampleData[d] = {otherName:otherName, demName:winnerName, repName:loserName,partyCount:3, dem:winPercent, demVotes:votes, repVotes:loserVotes, rep:losePercent, thirdPartyOne:thirdParty, thirdPartyVotes:thirdVotes, thirdVotes:thirdPlacePercent, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
+                        } else {
+                            sampleData[d] = {otherName:otherName, demName:loserName, repName:winnerName,partyCount:3, dem:losePercent, demVotes:loserVotes, repVotes:votes, rep:winPercent, thirdPartyOne:thirdParty, thirdPartyVotes:thirdVotes, thirdVotes:thirdPlacePercent, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
+                        }
+                    } else {
+                        if(party == "democrat") {
+                            sampleData[d] = {otherName:otherName, demName:winnerName, repName:loserName,partyCount:4, dem:winPercent, demVotes:votes, repVotes:loserVotes, rep:losePercent, other:otherPercent, otherVotes:otherVotes, thirdPartyOne:thirdParty, thirdPartyVotes:thirdVotes, thirdVotes:thirdPlacePercent, color:d3.interpolate("#FFFFFF", "#0015BC")(winningPercent)};
+                        } else {
+                            sampleData[d] = {otherName:otherName, demName:loserName, repName:winnerName,partyCount:4, dem:losePercent, demVotes:loserVotes, repVotes:votes, rep:winPercent, other:otherPercent, otherVotes:otherVotes, thirdPartyOne:thirdParty, thirdPartyVotes:thirdVotes, thirdVotes:thirdPlacePercent, color:d3.interpolate("#FFFFFF", "#E9141D")(winningPercent)};
+                        }
+                    }
                 }
             });
     /* draw states on id #statesvg */   
